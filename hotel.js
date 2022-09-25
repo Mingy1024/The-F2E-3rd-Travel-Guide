f6bd84c0225091f084bca0ec8efde95fc3fedc08
Mingy1024 <s2142717@gmail.com>
2022-09-25 20:35:27 +0800

    完成找旅宿頁面搜尋功能

--- hotel.js
+++ hotel.js
@@ -1,10 +1,16 @@
 
 // 選擇器設定
+const hotelCategory = {};
 const hotelList = document.querySelector('.hotelList');
+const citySelect = document.querySelector('.citySelect');
+const categorySelect = document.querySelector('.categorySelect');
+const txt = document.querySelector('.txt');
 const moreData = document.querySelector('.moreData');
+const send = document.querySelector('.send');
 let dataNum = 30;
 // ------ 初始化
 function init(){
+    getAllHotel();
     getOriginData();
 }
   
@@ -38,10 +44,53 @@ function getAuthorizationHeader() {
       });
 }
 
+send.addEventListener("click",function(e){
+		dataNum = 30;
+		renderData();
+})
+
 moreData.addEventListener("click",function(e){
     dataNum +=20;
-    getOriginData();
+		if(citySelect.value == "" && categorySelect.value == "" && txt.value == ""){
+			getOriginData();
+		}
+		else{
+			renderData();
+		}
 })
+
+function getAllHotel(){
+    axios.get(`https://tdx.transportdata.tw/api/basic/v2/Tourism/Hotel?%24format=JSON`,{
+        headers: getAuthorizationHeader()
+    })
+    .then((res) =>{
+        const thisData = res.data;
+        thisData.forEach((item) =>{
+            if(item.Class == undefined){
+                return;
+            }
+            else if(hotelCategory[item.Class] == undefined){
+                hotelCategory[item.Class] = 1;
+            }
+            else{
+                hotelCategory[item.Class] +=1;
+            }
+        })
+        renderCategory();
+    })
+    .catch((err) =>{
+        console.log(err);
+    })
+}
+
+function renderCategory(){
+    const hotelCategoryAry = Object.keys(hotelCategory);
+    let str = `<option selected class="d-none" value="">找分類</option>`;
+    hotelCategoryAry.forEach((item) =>{
+        str += `<option value="${item}">${item}</option>`;
+    })
+    categorySelect.innerHTML = str;
+}
 
 function getOriginData(){
     axios.get(`https://tdx.transportdata.tw/api/basic/v2/Tourism/Hotel?%24top=${dataNum}&%24format=JSON`,{
@@ -77,5 +126,59 @@ function getOriginData(){
     })
     .catch((err) =>{
         console.log(err);
+    })
+}
+
+function renderData(){
+    const city = citySelect.value;
+    const category = categorySelect.value;
+    const keyWord = txt.value;
+    let count = 0;
+    axios.get(`https://tdx.transportdata.tw/api/basic/v2/Tourism/Hotel/${city}?%24filter=contains(Class,'${category}')%20and%20contains(HotelName,'${keyWord}')&%24top=${dataNum}&%24format=JSON`,{
+        headers: getAuthorizationHeader()
+    })
+    .then(function(res){
+        const thisData =res.data;
+        let str = "";
+        thisData.forEach(function(item){
+						if(item.HotelName == "雀客旅館台北信義" || item.HotelName == "達觀農場"){
+							return;
+						}
+            else if(item.Picture.PictureUrl1 !== undefined){
+                str += `<div class="col">
+                            <div class="card w-100 h-100">
+                              <img
+                                src="${item.Picture.PictureUrl1}"
+                                class="card-img-top"
+                                alt="${item.Picture.PictureDescription1}"
+                                style="height: 200px; object-fit: cover;"
+                              />
+                              <div class="card-body p-20">
+                                <h5 class="card-title fw-bold lh-14 mb-12">${item.HotelName}</h5>
+                                <p class="card-text text-gray mb-12">
+                                  <i class="fa-solid fa-location-dot pe-2"></i>${item.City}
+                                </p>
+                                <span class="badge rounded-pill bg-primary text-gray"
+                                  >${item.Class}</span
+                                >
+                              </div>
+                            </div>
+                        </div>`
+                count += 1;
+            }
+        })
+        if(count <= 8){
+        	document.querySelector('.vector-2').classList.add('d-none');
+        	document.querySelector('.vector-3').classList.remove('d-lg-block');
+        }
+				else{
+					document.querySelector('.vector-2').classList.remove('d-none');
+          document.querySelector('.vector-3').classList.add('d-lg-block');
+				}
+        hotelList.innerHTML = str;
+    })
+    .catch((err) =>{
+        console.log(err);
+				alert("選擇城市!");
     })
 }
